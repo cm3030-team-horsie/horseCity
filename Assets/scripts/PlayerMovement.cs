fc773549e60b6e20100f0ceff211992f8ce63d74
PaulMarsh-UoL <pm286@student.london.ac.uk>
2025-05-17 23:17:14 -0700

    Basic WASD player movement and camera tracking

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal"); // 'A' and 'D' keys
+        float moveZ = Input.GetAxisRaw("Vertical");   // 'W' and 'S' keys
+
+        Quaternion rotation = Quaternion.Euler(0f, 45f, 0f);
+
+        Vector3 move = (rotation * new Vector3(moveX, 0f, moveZ)).normalized;
+        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
---
+++ Assets/scripts/PlayerTracking.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class PlayerTracking : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset = new Vector3(-8f, 8f, -8f);
+
+    private Camera cam;
+
+    private void Awake()
+    {
+        cam = GetComponent<Camera>();
+
+        cam.orthographic = true;
+        cam.orthographicSize = 10f;
+
+        transform.rotation = Quaternion.Euler(30f, 45f, 0f);
+    }
+
+    private void LateUpdate()
+    {
+        if (player != null)
+        {
+            transform.position = player.position + offset;
+        }
+    }
+}
