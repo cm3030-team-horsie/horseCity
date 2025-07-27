ce0f02cfd22c228b3837d05605d9f0fc24179093
PaulMarsh-UoL <pm286@student.london.ac.uk>
2025-07-26 21:16:14 -0700

    Added easyroads3d demo assets
    https://assetstore.unity.com/packages/tools/terrain/easyroads3d-demo-project-105045

---
+++ Assets/EasyRoads3D/Editor/ERPackageUpgrade.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ERPackageUpgrade : Editor
+{
+	[InitializeOnLoadMethod]
+	static void ERPackageUpgradeInit()
+	{
+
+#if UNITY_2022
+
+		if (EditorPrefs.GetString("unityVersion") != "Unity2022_b9")
+		{
+			//EditorPrefs.SetString("unityVersion", "Unity2022_b9"); 
+			//AssetDatabase.ImportPackage("Assets/EasyRoads3D/EasyRoads3Dv3.3b9-Unity2022.unitypackage", false);
+			//Debug.Log("EasyRoads3D Demo packages upgraded to Unity 2022");
+		}
+
+#elif UNITY_2021_2_OR_NEWER
+
+		if (EditorPrefs.GetString("unityVersion") != "Unity2021_2_b9")
+		{
+			EditorPrefs.SetString("unityVersion", "Unity2021_2_b9");
+			AssetDatabase.ImportPackage("Assets/EasyRoads3D/EasyRoads3Dv3.3b9-Unity2021.2+.unitypackage", false);
+			Debug.Log("EasyRoads3D Demo packages upgraded to Unity 2021.2+");
+		}
+
+#endif
+
+	}
+
+}
--- Assets/EasyRoads3D/SRP Support Packages/_Readme.txt
+++ Assets/EasyRoads3D/SRP Support Packages/_Readme.txt
@@ -3,9 +3,7 @@ EasyRoads3D SRP SUPPORT:
 
 The packages in this folder are Render Pipeline related.
 
-URP or HDRP packages for the installed version will be auto imported. This includes materials for the two provided example road materials
-
-Materials that still appear in pink can be upgraded using the Unity Render Pipeline Converter tool.  
+URP or HDRP packages for the installed version will be auto imported.
 
 There will be a notification when no matching package could be found. In that case please contact us.
 
