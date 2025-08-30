24a2f28906bbfb4ead824b1608f60faf14a5d22b
PaulMarsh-UoL <pm286@student.london.ac.uk>
2025-08-29 17:12:14 -0700

    Added text (Press "space" to escape trailer)

---
+++ Assets/scripts/UI/EscapeTextUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using HorseCity.Core;
+
+public class EscapeTextUI : MonoBehaviour
+{
+    private Text textComponent;
+
+    void Awake()
+    {
+        textComponent = GetComponent<Text>();
+    }
+
+    void OnEnable()
+    {
+        GameManager.OnGameStateChanged += HandleGameStateChanged;
+        UpdateTextVisibility(GameManager.Instance.GetGameState());
+    }
+
+    void OnDisable()
+    {
+        GameManager.OnGameStateChanged -= HandleGameStateChanged;
+    }
+
+    void HandleGameStateChanged(GameState newState)
+    {
+        UpdateTextVisibility(newState);
+    }
+
+    void UpdateTextVisibility(GameState state)
+    {
+        bool shouldBeVisible = (state == GameState.WaitingToStart);
+
+        if (textComponent != null)
+        {
+            textComponent.enabled = shouldBeVisible;
+        }
+    }
+}
